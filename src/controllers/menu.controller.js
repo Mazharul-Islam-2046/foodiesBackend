4c32c03db504188ac034891e1b52d6c98e777330
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2025-05-12 10:53:01 +0600

    fixed the filter controller

--- src/controllers/menu.controller.js
+++ src/controllers/menu.controller.js
@@ -40,7 +40,8 @@ export const getAllMenuItems = asyncHandler(async (req, res) => {
 export const filterMenuItems = asyncHandler(async (req, res) => {
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
-    const filters = req.body;
+    const filters = req.query;
+    console.log("Backend Filters",filters)
     const sortBy = req.query.sortBy;
 
 
